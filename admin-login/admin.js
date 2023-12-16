d82493c1d5b47303feb721b95376a09f818c6636
alveenaabidi <126185391+alveenaabidi@users.noreply.github.com>
2023-12-16 21:13:12 +0530

    Add files via upload

---
+++ admin-login/admin.js
@@ -0,0 +1,8 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Your code for handling the login button click event goes here
+});
+
+document.querySelector('button[type="button"]').addEventListener('click', function() {
+    // Your code for handling the cancel button click event goes here
+});
